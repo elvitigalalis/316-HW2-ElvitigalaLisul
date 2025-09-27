0d2834c1f3692c1a4d972e9648938264e06a996a
elvitigalalis <elvitigalalis@gmail.com>
2025-09-26 22:50:15 -0400

    Modify delete list modal to not be tab-able as per HW-1

--- src/components/DeleteListModal.jsx
+++ src/components/DeleteListModal.jsx
@@ -1,39 +1,51 @@
 import React, { Component } from 'react';
 
 export default class DeleteListModal extends Component {
+    getPlaylistName = () => {
+        const { listKeyPair } = this.props;
+        return listKeyPair ? listKeyPair.name : "";
+    }
+
     render() {
-        const { listKeyPair, deleteListCallback, hideDeleteListModalCallback } = this.props;
-        let name = "";
-        if (listKeyPair) {
-            name = listKeyPair.name;
-        }
+        const { deleteListCallback, hideDeleteListModalCallback } = this.props;
+        const playlistName = this.getPlaylistName();
+
         return (
-            <div 
-                class="modal" 
-                id="delete-list-modal" 
-                data-animation="slideInOutLeft">
-                    <div class="modal-root" id='verify-delete-list-root'>
-                        <div class="modal-north">
-                            Delete playlist?
-                        </div>
-                        <div class="modal-center">
-                            <div class="modal-center-content">
-                                Are you sure you wish to permanently delete the {name} playlist?
-                            </div>
-                        </div>
-                        <div class="modal-south">
-                            <input type="button" 
-                                id="delete-list-confirm-button" 
-                                class="modal-button" 
-                                onClick={deleteListCallback}
-                                value='Confirm' />
-                            <input type="button" 
-                                id="delete-list-cancel-button" 
-                                class="modal-button" 
-                                onClick={hideDeleteListModalCallback}
-                                value='Cancel' />
+            <div
+                className="modal"
+                id="delete-list-modal"
+                data-animation="slideInOutLeft"
+                tabIndex={-1}
+            >
+                <div className="modal-root" id="verify-delete-list-root">
+                    <div className="modal-north">
+                        Delete Playlist?
+                    </div>
+
+                    <div className="modal-center">
+                        <div className="modal-center-content">
+                            Are you sure you want to permanently delete&nbsp;
+                            <span className="modal-highlight">{playlistName}</span>?
                         </div>
                     </div>
+
+                    <div className="modal-south">
+                        <input
+                            type="button"
+                            id="delete-list-confirm-button"
+                            className="modal-button"
+                            onClick={deleteListCallback}
+                            value="Confirm"
+                        />
+                        <input
+                            type="button"
+                            id="delete-list-cancel-button"
+                            className="modal-button"
+                            onClick={hideDeleteListModalCallback}
+                            value="Cancel"
+                        />
+                    </div>
+                </div>
             </div>
         );
     }
